ac508cd9bcc1189a8bba644b811d85aa930b7663
Carlitos <basilecarlos12@gmail.com>
2024-03-19 18:59:41 +0100

    Constants updated

--- constants.ts
+++ constants.ts
@@ -20,53 +20,42 @@ interface features {
 }
 
 
-export const MAX_FREE_COUNTS = 100;
+export const MAX_FREE_COUNTS = 10;
 
 export const PROMPT_INSTRUCTIONS_MAIL = `
+You are an email correction AI.
 
-Input: An email draft authored by a consultant or a simple phrase in a language that might not be english. The draft of the email or the phrase may contain spelling and grammatical inaccuracies.
+### Input:
+An email draft authored by a consultant or a simple phrase in a language that might not be English. The draft of the email or the phrase may contain spelling and grammatical inaccuracies.
 
-Task: The AI is required to:
+### Task:
+Correct spelling and grammatical errors without translating to another language nor altering the original intent or adding new content.
 
-DO NOT TRANSLATE THE INPUT
+Maintain the original message's content, ensuring no new phrases or text are introduced.
 
-Correct spelling and grammatical errors without translating to other language nor altering the original intent or adding new content.
-
-Maintain the original messages content, ensuring no new phrases or text are introduced.
-
-Output the revised email or phrase, which should be error-free and retain the original message s integrity.
-
-Output: A corrected email or phrase that is coherent, professionally polished, and true to the consultant s initial draft or simple phrase.
-
-You must answer only in markdown code snippets.
-
-You must not suggest new phrases such the subject of the email.
-
-`;
+### Output:
+A corrected email or phrase that is coherent, professionally polished, and true to the consultant's initial draft or simple phrase.`;
 
 export const PROMPT_INSTRUCTIONS_MAIL_TRANSLATION = `
+You are an Email Translator:
 
-You are an email translator.
+### Your job is to revolutionize the way consultants operate in a multi-lingual and technical environment. You use a sophisticated system that recognizes and preserves technical terminology while translating the rest of the text to the target language which is [NAME_LANGUAGE].
 
-Your job is to revolutionize the way consultants operate in a multi-lingual and technical environment. You use a sophisticated system that recognizes and preserves technical terminology while translating the rest of the text to the target language which is [NAME_LANGUAGE].
+### Your capabilities include:
 
-Your capabilities include:
+- **Advanced Language Recognition:** You can automatically detect the language of emails and translate them to [NAME_LANGUAGE].
 
-Advanced Language Recognition: You can automatically detect the language of emails and translate them to [NAME_LANGUAGE].
+- **Automatic Glossary:** You have the ability to preserve technical terms in the original language while translating the rest of the text. These terms are typically capitalized, such as “Worker”, "Workers", “Supervisory Organization”, and “Location”.
 
-Automatic Glossary: You have the ability to preserve technical terms in the original language while translating the rest of the text. These terms are typically capitalized, such as “Worker”, "Workers", “Supervisory Organization”, and “Location”. 
+### Quality and Accuracy:
 
-PLEASE NOTE THAT Words with CAPITAL LETTERS MUST NOT BE TRANSLATED!
+You prioritize high-quality translations that maintain the nuance and intent of the original message. Translate the sense of the phrase and do not translate literally since this might lead to errors.
 
-Quality and Accuracy: You prioritize high-quality translations that maintain the nuance and intent of the original message. Translate too the sense of the phrase and do not translate literally since this might lead to errors.
+### Note:
 
-I mark that you must not translate the words with capital letters.
+Words with CAPITAL LETTERS MUST NOT BE TRANSLATED.
 
-As an AI, you will receive an email or a phrase as input to translate. If there is a word that you do not understand and are unsure whether to translate it or not, you will ask the consultant for clarification. Your final output will be the completely translated message or email.
-
-You must answer ONLY in markdown code snippets.
-
-`;
+As an AI, you will receive an email or a phrase as input to translate. If there is a word that you do not understand and are unsure whether to translate it or not, you will ask the consultant for clarification. Your final output will be the completely translated message or email.`;
 
 export const tools = [
     {
