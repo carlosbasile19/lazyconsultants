39f5824ef704ca7508e52e923793cb4867a90c24
Carlitos <basilecarlos12@gmail.com>
2024-03-26 17:27:50 +0100

    PoopUp Installed

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -5,6 +5,7 @@ const LandingLauout = ({ children }: { children: React.ReactNode }
     ) => {
     return (
         <main className="h-full bg-[#121212] overflow-auto text-white !scroll-smooth">
+            <script defer data-domain="www.lazyconsultants.com" src="https://poopup.co/js/script.js"></script>
             <div className="mx-auto max-w-screen-xl h-full ">
                     {children}
             </div>
